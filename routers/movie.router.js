d990d1c4718afc355e585ea4cf77783d1ea16a0e
iammahesh09 <iammahesh.nlr@gmail.com>
2018-08-15 16:00:48 +0530

    Fileupload

--- routers/movie.router.js
+++ routers/movie.router.js
@@ -2,9 +2,28 @@ const express = require('express');
 const router = express.Router();
 let MovieCtrl = require('../controllers/movie.ctrl');
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploadData");
+    },
+    filename: function (req, file, cb) {
+        let filename = Date.now() + "-" + file.originalname;
+        req.body.Poster = filename;
+        cb(null, filename);
+    }
+});
+var upload = multer({
+    storage: storage
+})
+
+
+
+
+
 router.get('', MovieCtrl.get);
 router.get('/:id', MovieCtrl.getId);
-router.post('/create', MovieCtrl.create);
+router.post('/create', upload.single("Poster"), MovieCtrl.create);
 router.delete('/:id', MovieCtrl.delete);
 router.patch('/:id', MovieCtrl.update);
 
